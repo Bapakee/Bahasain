28d86bf2a1bb930a21a9316c7f22e621c5eb82e2
AkmalKv <akmalkv4@gmail.com>
2024-11-20 20:42:18 +0700

    tes1

--- Mobile-Development/app/src/main/java/com/dicoding/bahasain/ui/learn/LearnFragment.kt
+++ Mobile-Development/app/src/main/java/com/dicoding/bahasain/ui/learn/LearnFragment.kt
@@ -1,4 +1,4 @@
-package com.dicoding.bahasain.ui.notifications
+package com.dicoding.bahasain.ui.learn
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.dicoding.bahasain.databinding.FragmentNotificationsBinding
+import com.dicoding.bahasain.databinding.FragmentDashboardBinding
 
-class NotificationsFragment : Fragment() {
+class LearnFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val learnViewModel =
+            ViewModelProvider(this).get(LearnViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        learnViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
