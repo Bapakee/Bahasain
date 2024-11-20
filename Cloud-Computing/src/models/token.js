cf2b97d570ab2d66a1944328d39fa6a0ef2d63a6
Hafi <mufaddholhafi26@gmail.com>
2024-11-20 14:50:02 +0800

    <fix> error auth

--- Cloud-Computing/src/models/token.js
+++ Cloud-Computing/src/models/token.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
 
   Token.init(
     {
-      userId: {
+      user_id: {
         type: DataTypes.STRING,
         allowNull: false,
       },
--- Cloud-Computing/src/utils/tokenHelper.js
+++ Cloud-Computing/src/utils/tokenHelper.js
@@ -6,21 +6,21 @@ const { json } = require('body-parser');
 require('dotenv').config();
 
 const generateAccessToken = (user) => {
-  return jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '15m' });
+  return jwt.sign({ id: user.id, name: user.name }, process.env.JWT_SECRET, { expiresIn: '15m' });
 };
 
 const generateRefreshToken = async (user) => {
-  const token = jwt.sign({ id: user.id,username : user.username }, process.env.JWT_REFRESH_SECRET, {
+  const token = jwt.sign({ id: user.id,name : user.name }, process.env.JWT_REFRESH_SECRET, {
     expiresIn: '30d',
   });
   await Token.destroy({
     where: {
-      userId: user.id,
+      user_id: user.id,
       type: 'refresh',
     },
   });
   await Token.create({
-    userId:user.id,
+    user_id:user.id,
     type: 'refresh',
     token,
     expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 7 hari
@@ -29,17 +29,17 @@ const generateRefreshToken = async (user) => {
   return token;
 };
 const generateResetToken = async (user) => {
-  const userId=user.id
+  const user_id=user.id
   const token = crypto.randomBytes(10).toString('hex');
 
   await Token.destroy({
     where: {
-      userId,
+      user_id,
       type: 'reset',
     },
   });
   await Token.create({
-    userId,
+    user_id,
     type: 'reset',
     token,
     expiresAt: new Date(Date.now() + 3600000), // 1 jam
@@ -57,7 +57,7 @@ const validateToken = async (token, type) => {
     },
   });
 
-  return storedToken ? storedToken.userId : null;
+  return storedToken ? storedToken.user_id : null;
 };
   
 
