aa0eae53732d04c097edd680374b4b12a24692eb
AkmalKv <akmalkv4@gmail.com>
2024-11-22 00:40:05 +0700

    tes3

---
+++ Mobile-Development/app/src/main/java/com/bahasain/ui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.bahasain.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.dicoding.bahasain.R
+import com.dicoding.bahasain.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        navView.setupWithNavController(navController)
+    }
+}
--- Mobile-Development/app/src/main/java/com/dicoding/bahasain/ui/onboarding/OnBoardingItem.kt
+++
@@ -1,7 +0,0 @@
-package com.dicoding.bahasain.ui.onboarding
-
-data class OnBoardingItem(
-    val imageRes: Int,
-    val title: String,
-    val description: String
-)
