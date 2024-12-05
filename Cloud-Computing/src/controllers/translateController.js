61270cf0c4426819557dcfcd2b78ee396e487d16
HafiMufaddhol <mufaddholhafi26@gmail.com>
2024-12-05 18:44:32 +0800

    <feat> add translate api

--- Cloud-Computing/src/controllers/translateController.js
+++ Cloud-Computing/src/controllers/translateController.js
@@ -3,15 +3,20 @@ const { successResponse, errorResponse } = require('../utils/responseConsistency
 
 const getTranslate = async (req, res) => {
     const { text } = req.body;
+    console.log(text);
+    
     try {
         // Await the axios POST request to get the translation response
-        const translate = await axios.post('http://localhost:8000/translate', { text });
-
-        // Log the response from translation
-        console.log(translate.data);
+        const translate = await axios.post(`${process.env.TRANSLATE_URL}/translate`, { text });
+        const translated=translate.data.translation
+        const pos = await axios.post(`${process.env.POS_URL}/predict`, { text:translated });
+        const response = {
+            translate : translated,
+            pos :pos.data
+        }
 
         // Send the success response with translation data
-        successResponse(res, translate.data, 'Word fetched successfully');
+        successResponse(res, response, 'Word fetched successfully');
     } catch (error) {
         // Handle error and send failure response
         errorResponse(res, error, 'Error fetching word', 500);
